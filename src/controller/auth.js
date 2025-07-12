0b0342192e5d2caa389a668814e9941571a327db
renchooz <rjsharma.rs967@gmail.com>
2025-07-12 18:51:07 +0530

    done

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -62,8 +62,8 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+      secure: true,
+      sameSite: "None",
     });
     return res.status(200).json({ message: "logged out succesfully" });
   } catch (error) {
--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -7,8 +7,8 @@ export const checkAuth = async(userId,res)=>{
         })
         res.cookie("token",token,{
             httpOnly: true,       // Prevents access from JavaScript
-            secure: process.env.NODE_ENV === "production",  // Sends cookie only on HTTPS
-            sameSite: "Lax",   // Protects against CSRF
+            secure: true,  // Sends cookie only on HTTPS
+            sameSite: "None",   // Protects against CSRF
             maxAge: 7 * 24 * 60 * 60 * 1000,  // 7 days in ms
         })
         return token;

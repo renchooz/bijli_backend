8d0d87844f2b7f401b3991d6748ee107a8672066
renchooz <rjsharma.rs967@gmail.com>
2025-07-12 18:43:26 +0530

    cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://spiffy-brigadeiros-344677.netlify.app"],
   credentials: true
 }));
 
